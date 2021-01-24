10534499d1e840d3e4500b659b4c1a6dfe680c24
gerasimovs <mrteos@gmail.com>
2021-01-24 18:27:48 +0700

    Create ProductSection.php

---
+++ src/Entities/Crms/ProductSection.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace GerasimovS\Bitrix24\Entities\Crms;
+
+use GerasimovS\Bitrix24\Traits\Resource;
+
+/**
+ * Class ProductSection
+ *
+ * @link https://dev.1c-bitrix.ru/rest_help/crm/product_section/index.php
+ */
+class ProductSection
+{
+    use Resource;
+
+    public $methodGroup = 'crm.productsection';
+}
